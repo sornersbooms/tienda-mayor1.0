76b214950453924b3c2cfe26feeabe6e5a4ba298
sornersbooms <oscardanielyepez777@gmail.com>
2025-09-24 05:42:01 -0500

    7

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   process.env.FRONTEND_URL,
-  'https://gallant-stillness-production.up.railway.app'
+  'https://gallant-stillness-production.up.railway.app',
+  'https://tiendamayor.com/'
 ].filter(Boolean);
 
 app.use(cors({
